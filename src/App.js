ded7186496c3a66404c3d95c5a26179ae10a06c7
nmrndll <81620556+nmrndll@users.noreply.github.com>
2021-09-27 12:33:16 +0800

    update app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import './App.css';
 
-function App() {
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-<h1>Weather App</h1>
-<footer>
-  This project is coded by{" "}<a href="https://github.com/nmrndll" target="_blank">
-    Naomi Randall</a>{" "}and is open-sourced on{" "}
-<a href="https://github.com/nmrndll/react-weather-app" target="_blank">Github
-</a>
-</footer>
+      <div className="container">
+        <Weather defaultCity="London" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/nmrndll"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Naomi Randall
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/nmrndll/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://elastic-franklin-aa5196.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
